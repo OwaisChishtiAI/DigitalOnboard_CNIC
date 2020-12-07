bbe3a44ef4c6041eb7b384e172b177d9dd0528f5
Muhammad Danyal Jamil Chohan <56127751+D3nii@users.noreply.github.com>
2020-12-07 20:58:20 +0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+from Test.Cnic_reality import check_reality
+from flask import Flask, Response, jsonify, request
+
+app = Flask(__name__)
+
+
+@app.route('/detectcnic', methods=["POST"])
+def detectCNIC():
+    frame = request.form["image"]
+
+    temp = check_reality(frame).returnLabel()
+
+    if (temp):
+        return "", 200
+
+    return "", 403
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
