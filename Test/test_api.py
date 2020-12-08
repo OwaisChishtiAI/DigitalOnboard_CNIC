46166b863c65bfdb7aa6b00735c08525907d907a
Muhammad Danyal Jamil Chohan <56127751+D3nii@users.noreply.github.com>
2020-12-08 19:08:48 +0500

    Update test_api.py

--- Test/test_api.py
+++ Test/test_api.py
@@ -4,17 +4,19 @@ import requests
 from pathlib import Path
 
 # Passing in a real face Image
-with open(str(Path(__file__).resolve().parent) + '/Images/1.png', 'rb') as img_file:
+with open(str(Path(__file__).resolve().parent) + '/images/1.png', 'rb') as img_file:
     my_string = base64.b64encode(img_file.read())
 
-r = requests.post("http://0.0.0.0:5000/detectcnic", data={'image': my_string})
+r = requests.post("http://0.0.0.0:5000/v2/detectcnic",
+                  data={'image': my_string})
 
 print(r.status_code)
 
 # Passing in a fake Image
-with open(str(Path(__file__).resolve().parent) + '/Images/2.png', 'rb') as img_file:
+with open(str(Path(__file__).resolve().parent) + '/images/2.png', 'rb') as img_file:
     my_string = base64.b64encode(img_file.read())
 
-r = requests.post("http://0.0.0.0:5000/detectcnic", data={'image': my_string})
+r = requests.post("http://0.0.0.0:5000/v2/detectcnic",
+                  data={'image': my_string})
 
 print(r.status_code)
